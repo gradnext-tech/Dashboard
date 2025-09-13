3156080c38c7a0ce7d28724446b789b43e14c065
Apoorv <apoorobot21@gmail.com>
2025-09-13 19:26:27 +0530

    Prod

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Remove experimental appDir as it's stable in Next.js 14
+  output: 'standalone',
+  experimental: {
+    serverComponentsExternalPackages: ['googleapis']
+  }
 }
 
 module.exports = nextConfig
